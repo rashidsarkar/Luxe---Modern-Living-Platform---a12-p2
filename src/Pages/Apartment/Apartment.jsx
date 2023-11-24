006718c910bcc6abb51ce5bc1d126720ebef0e46
rashidsarkar <rashidrock558@gmail.com>
2023-11-24 15:49:38 +0600

    feat: Implement Apartment component with room exploration
    Implemented the Apartment component, allowing users to explore luxurious apartments. Utilized the ApartmentBanar component for a visually appealing banner with a title and description.
    
    Integrated the FeatureSection component to display individual rooms, fetching data from the server with pagination support. Implemented navigation buttons for easy exploration.
    
    Utilized the useQuery hook for data fetching, CustomLoading for loading states, and ErrorMessage for error handling.

--- src/Pages/Apartment/Apartment.jsx
+++ src/Pages/Apartment/Apartment.jsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import useApartmentRoom from "../../API/useApartmentRoom";
+import axiosInstancePublic from "../../AxiosAPI/axiosInstance";
 import ApartmentBanar from "../../Components/App/ApartmentBanar/ApartmentBanar";
 import CustomLoading from "../../Components/CustomLoading";
 import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
@@ -6,11 +8,44 @@ import SectionHeading from "../../Components/SectionHeading/SectionHeading";
 import SectionTitle from "../../TextEffectComponents/BannerDecpt/SectionTitle/SectionTitle";
 import useAuth from "../../hooks/useAuth";
 import FeatureSection from "../Home/Feature/FeatureSection";
+import { useState } from "react";
 
 function Apartment() {
   const { user } = useAuth();
-  const { roomsData, isLoading, isError, error } = useApartmentRoom();
+  // const { roomsData, isLoading, isError, error } = useApartmentRoom();
   // console.log(singleRoomData);
+  const [limit, setLimit] = useState(6);
+  const [page, setPage] = useState(1);
+
+  const getData = async () => {
+    const myData = await axiosInstancePublic.get(
+      `/api/apartmentRooms?limit=${limit}&page=${page}`
+    );
+    return myData.data;
+  };
+
+  const {
+    data: roomsData,
+    isLoading,
+    error,
+    refetch,
+    isError, // Get the refetch function
+  } = useQuery({
+    queryFn: getData, // Call the function
+    // queryKey: ["all-assignments", selectedDifficulty, limit, page],
+    queryKey: ["singleRoomData", page], // Include selectedDifficulty in the query key
+  });
+  let totalPage = Math.ceil(roomsData?.count / limit);
+  const handlePrevious = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  const handleNext = () => {
+    if (page < totalPage) {
+      setPage(page + 1);
+    }
+  };
 
   if (isLoading) return <CustomLoading></CustomLoading>;
   if (isError) return <ErrorMessage error={error}></ErrorMessage>;
@@ -28,9 +63,47 @@ function Apartment() {
         {/* //TODO -  room databse teke ante hobe */}
         {/* //TODO -  pagination koira dekate hobe */}
         <div className="grid grid-cols-3 py-9">
-          {roomsData.map((room) => {
+          {/* {roomsData.map((room) => {
             return <FeatureSection key={room._id} room={room}></FeatureSection>;
-          })}
+          })} */}
+
+          {roomsData.result.map((room) => (
+            <FeatureSection key={room._id} room={room} />
+          ))}
+        </div>
+        <div className="flex justify-center ">
+          <div className="join join-vertical lg:join-horizontal">
+            <div className="border-2 rounded-none join border-primary">
+              <button
+                onClick={handlePrevious}
+                className="btn join-item btn-ghost"
+              >
+                {"<<"}
+              </button>
+              {Array(totalPage)
+                .fill(0)
+                .map((item, idx) => {
+                  const pageNum = idx + 1;
+                  return (
+                    <button
+                      key={idx + 1}
+                      onClick={() => setPage(pageNum)}
+                      className={`${
+                        pageNum === page
+                          ? "btn join-item btn-primary"
+                          : "btn join-item btn-ghost"
+                      } `}
+                    >
+                      {` ${pageNum} `}
+                    </button>
+                  );
+                })}
+
+              <button onClick={handleNext} className="btn join-item btn-ghost">
+                {">>"}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
