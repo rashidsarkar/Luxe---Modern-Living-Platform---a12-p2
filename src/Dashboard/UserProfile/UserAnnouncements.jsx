0762469766c75070ce38f067fcbad34e075a2a44
rashidsarkar <rashidrock558@gmail.com>
2023-11-25 15:42:04 +0600

    feat: Implement UserAnnouncements component
    - Created the  component to display user-specific announcements.
    - Utilized the  hook to fetch announcement data.
    - Rendered AnnouncementCard components for each announcement in a responsive grid layout.
    - Added loading and error handling using  and  components.

--- src/Dashboard/UserProfile/UserAnnouncements.jsx
+++ src/Dashboard/UserProfile/UserAnnouncements.jsx
@@ -1,13 +1,27 @@
+import useUserAnnounce from "../../API/UserAPI/useUserAnnounce";
+import CustomLoading from "../../Components/CustomLoading";
+import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
 import AnnouncementCard from "./announcementCard/AnnouncementCard";
 
 function UserAnnouncements() {
+  const { announceData, error, isError, isLoading } = useUserAnnounce();
+
+  if (isLoading) return <CustomLoading></CustomLoading>;
+  if (isError) return <ErrorMessage error={error}></ErrorMessage>;
+  console.log(announceData);
   return (
     <div>
       <p>this is header </p>
       <div className="grid grid-cols-1 gap-2 p-6 md:grid-cols-2 lg:grid-cols-3">
-        <AnnouncementCard></AnnouncementCard>
-        <AnnouncementCard></AnnouncementCard>
-        <AnnouncementCard></AnnouncementCard>
+        {announceData.map((announce) => {
+          return (
+            <AnnouncementCard
+              title={announce.title}
+              description={announce.description}
+              key={announce._id}
+            ></AnnouncementCard>
+          );
+        })}
       </div>
     </div>
   );
--- src/Dashboard/UserProfile/announcementCard/AnnouncementCard.jsx
+++ src/Dashboard/UserProfile/announcementCard/AnnouncementCard.jsx
@@ -1,25 +1,30 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { IoMdMegaphone } from "react-icons/io";
 import { MdReadMore } from "react-icons/md";
 
-function AnnouncementCard() {
+function AnnouncementCard({ title, description }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
 
   const handleReadMore = () => {
     setShowFullDescription(!showFullDescription);
   };
+
+  // Display either a truncated or full description based on showFullDescription state
+  const displayedDescription = showFullDescription
+    ? description
+    : description.slice(0, 50) + (description.length > 50 ? "..." : "");
+
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <IoMdMegaphone className="mb-3 text-gray-500 text-7xl dark:text-gray-400" />
       <a href="#">
         <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          Need help with your Claim?
+          {title}
         </h5>
       </a>
       <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-        {showFullDescription
-          ? "Go to this step by step guideline process on how to certify for your weekly benefits."
-          : "A short preview of the announcement description..."}
+        {displayedDescription}
       </p>
       <button
         onClick={handleReadMore}
